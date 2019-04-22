4aa013abfa3e66d0f0751613528a353dbe9fd998
luanlpg <luanlpg13@gmail.com>
2019-04-22 16:27:47 -0300

    PORT

--- runserver.py
+++ runserver.py
@@ -15,4 +15,5 @@ def hello():
     return render_template("index.html")
 
 if __name__ == '__main__':
-            app.run(host='0.0.0.0', port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
